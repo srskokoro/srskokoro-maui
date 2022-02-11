32c7d0c654fda9f6d62e0e0227d7860b6ec5770a
netaneru <netaneru3@gmail.com>
2022-02-11 19:02:13 +0800

    Get the type of a variable or value without needing to box value types

---
+++ Kokoro/Util/Var.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace Kokoro.Util;
+
+public static class Var {
+
+	/// <summary>
+	/// Gets the type of a variable or value without needing to box value types.
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
+	public static Type TypeOf<T>(in T var) {
+		return typeof(T).IsValueType || var is null ? typeof(T) : var.GetType();
+	}
+}
