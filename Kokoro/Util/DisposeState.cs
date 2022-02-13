4a171e935de190188f195760cc93ad29b0a7a142
netaneru <netaneru3@gmail.com>
2022-02-13 13:06:06 +0800

    Added some aliases

--- Kokoro/Util/DisposeState.cs
+++ Kokoro/Util/DisposeState.cs
@@ -4,4 +4,14 @@ internal enum DisposeState : int {
 	NotDisposed = 0,
 	DisposeRequested = 1,
 	DisposedFully = 2|DisposeRequested,
+
+#pragma warning disable CA1069 // Enums values should not be duplicated
+	#region Aliases
+
+	DisposedPartially = DisposeRequested,
+	Disposed = DisposedPartially,
+	DisposeCommitted = DisposedFully,
+
+	#endregion
+#pragma warning restore CA1069 // Enums values should not be duplicated
 }
