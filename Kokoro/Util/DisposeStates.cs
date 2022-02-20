8b5c83a648a9cb3bb2ef4a28c1bbd9fb48924487
netaneru <netaneru3@gmail.com>
2022-02-20 22:32:25 +0800

    Optimized

--- Kokoro/Util/DisposeStates.cs
+++ Kokoro/Util/DisposeStates.cs
@@ -15,54 +15,72 @@ internal static class DisposeStates {
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool IsNotDisposed(ref this DisposeState currentDisposeState) {
-		return currentDisposeState.VolatileRead() == DisposeState.None;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState.VolatileRead() == DisposeState.None ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool IsNotDisposed_NV(this DisposeState currentDisposeState) {
-		return currentDisposeState == DisposeState.None;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState == DisposeState.None ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool IsDisposed(ref this DisposeState currentDisposeState) {
-		return currentDisposeState.VolatileRead() != DisposeState.None;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState.VolatileRead() != DisposeState.None ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool IsDisposed_NV(this DisposeState currentDisposeState) {
-		return currentDisposeState != DisposeState.None;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState != DisposeState.None ? true : false;
 	}
 
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool CanHandleDisposeRequest(ref this DisposeState currentDisposeState) {
-		return currentDisposeState.VolatileRead() < DisposeState.Disposing_Flag;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState.VolatileRead() < DisposeState.Disposing_Flag ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool CanHandleDisposeRequest_NV(this DisposeState currentDisposeState) {
-		return currentDisposeState < DisposeState.Disposing_Flag;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState < DisposeState.Disposing_Flag ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool CannotHandleDisposeRequest(ref this DisposeState currentDisposeState) {
-		return currentDisposeState.VolatileRead() >= DisposeState.Disposing_Flag;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState.VolatileRead() >= DisposeState.Disposing_Flag ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool CannotHandleDisposeRequest_NV(this DisposeState currentDisposeState) {
-		return currentDisposeState >= DisposeState.Disposing_Flag;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
+		return currentDisposeState >= DisposeState.Disposing_Flag ? true : false;
 	}
 
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
 	public static bool HandleDisposeRequest(ref this DisposeState currentDisposeState) {
 		DisposeState oldDisposeState = currentDisposeState;
+		// Ternary operator returning true/false prevents redundant asm generation:
+		// See, https://github.com/dotnet/runtime/issues/4207#issuecomment-147184273
 		return oldDisposeState.CanHandleDisposeRequest_NV() && Interlocked.CompareExchange(
 			ref Unsafe.As<DisposeState, uint>(ref currentDisposeState)
 			, (uint)DisposeState.Disposing
 			, (uint)oldDisposeState
-		) == (uint)oldDisposeState;
+		) == (uint)oldDisposeState ? true : false;
 	}
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining | MethodImplOptions.AggressiveOptimization)]
