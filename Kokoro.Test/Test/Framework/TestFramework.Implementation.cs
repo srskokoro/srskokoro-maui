2715d280d3822de31c369be13030a41a818b52a0
netaneru <netaneru3@gmail.com>
2022-03-01 04:18:27 +0800

    Moved into its own file

--- Kokoro.Test/Test/Framework/TestFramework.Implementation.cs
+++ Kokoro.Test/Test/Framework/TestFramework.Implementation.cs
@@ -10,35 +10,3 @@ internal partial class TestFramework : XunitTestFramework {
 	protected override ITestFrameworkExecutor CreateExecutor(AssemblyName assemblyName)
 		=> new TestFrameworkExecutor(assemblyName, SourceInformationProvider, DiagnosticMessageSink);
 }
-
-internal class TestFrameworkExecutor : XunitTestFrameworkExecutor, IRandomizedTestFramework {
-
-	public TestFrameworkExecutor(
-		AssemblyName assemblyName,
-		ISourceInformationProvider sourceInformationProvider,
-		IMessageSink diagnosticMessageSink
-	) : base(assemblyName, sourceInformationProvider, diagnosticMessageSink) { }
-
-	protected override async void RunTestCases(
-		IEnumerable<IXunitTestCase> testCases,
-		IMessageSink executionMessageSink,
-		ITestFrameworkExecutionOptions executionOptions
-	) {
-		LoadLocalRandomState();
-		using var assemblyRunner = new XunitTestAssemblyRunner(TestAssembly, testCases, DiagnosticMessageSink, executionMessageSink, executionOptions);
-		RunSummary testSummary = await assemblyRunner.RunAsync();
-		SaveLocalRandomState(testSummary);
-	}
-
-	// --
-
-	private protected const string DateTimeSeedFile = @"test_start_dt_preserved_on_fail.dat";
-
-	private protected static void LoadLocalRandomState() {
-		IRandomizedTestFramework.LoadLocalRandomState(TestFrameworkConfig.DateTimeSeed, DateTimeSeedFile);
-	}
-
-	private protected static void SaveLocalRandomState(RunSummary testSummary) {
-		IRandomizedTestFramework.SaveLocalRandomState(testSummary, DateTimeSeedFile);
-	}
-}
