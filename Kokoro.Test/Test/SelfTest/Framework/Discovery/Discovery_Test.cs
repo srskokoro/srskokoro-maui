7680e39f28d44f6407dadee575c22dd37b25ea0b
netaneru <netaneru3@gmail.com>
2022-03-04 01:06:59 +0800

    Guard against namespace refactoring errors

---
+++ Kokoro.Test/Test/SelfTest/Framework/Discovery/Discovery_Test.cs
@@ -0,0 +1,33 @@
+﻿namespace Kokoro.Test.SelfTest.Framework.Discovery;
+
+using Kokoro.Test.Framework.Discovery;
+using Xunit.Sdk;
+
+public class Discovery_Test {
+
+	[Fact(DisplayName = $"All `{nameof(IXunitTestCaseDiscoverer)}` implementations have proper `TypeName` constants")]
+	public void T001() {
+		using var scope = new AssertionScope();
+
+		// --
+		{
+			const string expected = "TypeName";
+			// Guard only against one name refactoring, since all the others
+			// should be changed to match it also anyway.
+			nameof(TestFactDiscoverer.TypeName).Should()
+				.Be(expected, $"because it's currently being used in the test" +
+				$" method `{nameof(FactAttribute.DisplayName)}` (since, " +
+				$"`nameof({nameof(TestFactDiscoverer)}.{expected})` makes " +
+				$"legibility bad)");
+		}
+
+		// --
+		// Guard against namespace refactorings
+
+		typeof(TestFactDiscoverer).ToString().Should()
+			.Be(TestFactDiscoverer.TypeName);
+
+		typeof(TestTheoryDiscoverer).ToString().Should()
+			.Be(TestTheoryDiscoverer.TypeName);
+	}
+}
---
+++ Kokoro.Test/Test/SelfTest/Framework/TestFramework_Test.cs
@@ -0,0 +1,24 @@
+﻿namespace Kokoro.Test.SelfTest.Framework;
+
+using Kokoro.Test.Framework;
+
+public class TestFramework_Test {
+
+	[Fact(DisplayName = $"`{nameof(TestFramework)}` has proper `TypeName` constant")]
+	public void T001() {
+		using var scope = new AssertionScope();
+
+		// --
+		{
+			const string expected = "TypeName";
+			nameof(TestFramework.TypeName).Should()
+				.Be(expected, $"because it's currently being used in the test" +
+				$" method `{nameof(FactAttribute.DisplayName)}` (since, " +
+				$"`nameof({nameof(TestFramework)}.{expected})` makes " +
+				$"legibility bad)");
+		}
+
+		typeof(TestFramework).ToString().Should()
+			.Be(TestFramework.TypeName);
+	}
+}
