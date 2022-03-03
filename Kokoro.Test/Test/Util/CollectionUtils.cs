47cc242d1110506949ad21e8f3e5de4a7215c8d4
netaneru <netaneru3@gmail.com>
2022-03-03 17:13:12 +0800

    Made it our own

--- Kokoro.Test/Test/Util/CollectionUtils.cs
+++ Kokoro.Test/Test/Util/CollectionUtils.cs
@@ -1,12 +1,13 @@
 namespace Kokoro.Test.Util;
 
-// From, https://github.com/xunit/xunit/blob/2.4.1/src/xunit.execution/Sdk/Utility/CollectionExtensions.cs
-public static class CollectionExtensions {
+public static class CollectionUtils {
 
+	// From, https://github.com/xunit/xunit/blob/2.4.1/src/xunit.execution/Sdk/Utility/CollectionExtensions.cs
 	public static List<T> CastOrToList<T>(this IEnumerable<T> source) {
 		return source as List<T> ?? source.ToList();
 	}
 
+	// From, https://github.com/xunit/xunit/blob/2.4.1/src/xunit.execution/Sdk/Utility/CollectionExtensions.cs
 	public static T[] CastOrToArray<T>(this IEnumerable<T> source) {
 		return source as T[] ?? source.ToArray();
 	}
