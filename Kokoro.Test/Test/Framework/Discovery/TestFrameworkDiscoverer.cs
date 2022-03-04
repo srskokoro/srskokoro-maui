b211435d3be55f6a0921467c60df31cea0932012
netaneru <netaneru3@gmail.com>
2022-03-04 23:17:10 +0800

    Ensure that every `[Label]` is always paired with `[TestFact]` or `[TestTheory]`

---
+++ Kokoro.Test/Test/Framework/Discovery/TestFrameworkDiscoverer.cs
@@ -0,0 +1,41 @@
+﻿namespace Kokoro.Test.Framework.Discovery;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+internal class TestFrameworkDiscoverer : XunitTestFrameworkDiscoverer {
+
+	public TestFrameworkDiscoverer(
+		IAssemblyInfo assemblyInfo,
+		ISourceInformationProvider sourceProvider,
+		IMessageSink diagnosticMessageSink,
+		IXunitTestCollectionFactory? collectionFactory = null
+	) : base(assemblyInfo, sourceProvider, diagnosticMessageSink, collectionFactory) { }
+
+	protected override bool FindTestsForMethod(ITestMethod testMethod, bool includeSourceInformation, IMessageBus messageBus, ITestFrameworkDiscoveryOptions discoveryOptions) {
+		var method = testMethod.Method;
+
+		var labelAttribute = method.GetCustomAttributes(typeof(LabelAttribute)).FirstOrDefault();
+		if (labelAttribute is not null) {
+			var factAttribute = method.GetCustomAttributes(typeof(FactAttribute)).FirstOrDefault();
+			if (factAttribute is IReflectionAttributeInfo reflectFact
+					&& reflectFact.Attribute is not ITestFactAttribute) {
+				// Note that we don't fail when we don't have an `IReflectionAttributeInfo`,
+				// since the actual attribute might indeed be fulfilling the contract.
+				var errorMessage = $"[Label]-derived attribute must only be " +
+					$"given to a test method whose [Fact]-derived attribute " +
+					$"implements `{nameof(ITestFactAttribute)}`";
+
+				var testCase = new ExecutionErrorTestCase(
+					DiagnosticMessageSink,
+					discoveryOptions.MethodDisplayOrDefault(),
+					discoveryOptions.MethodDisplayOptionsOrDefault(),
+					testMethod, errorMessage
+				);
+
+				return ReportDiscoveredTestCase(testCase, includeSourceInformation, messageBus);
+			}
+		}
+
+		return base.FindTestsForMethod(testMethod, includeSourceInformation, messageBus, discoveryOptions);
+	}
+}
--- Kokoro.Test/Test/Framework/TestFramework._.cs
+++ Kokoro.Test/Test/Framework/TestFramework._.cs
@@ -1,10 +1,15 @@
 ﻿namespace Kokoro.Test.Framework;
 using System.Reflection;
 using Xunit.Sdk;
+using TestFrameworkDiscoverer = Discovery.TestFrameworkDiscoverer;
 
 internal partial class TestFramework : XunitTestFramework {
 
 	public TestFramework(IMessageSink messageSink) : base(messageSink) { }
+
+	protected override ITestFrameworkDiscoverer CreateDiscoverer(IAssemblyInfo assemblyInfo) {
+		return new TestFrameworkDiscoverer(assemblyInfo, SourceInformationProvider, DiagnosticMessageSink);
+	}
 
 	protected override ITestFrameworkExecutor CreateExecutor(AssemblyName assemblyName)
 		=> new TestFrameworkExecutor(assemblyName, SourceInformationProvider, DiagnosticMessageSink);
