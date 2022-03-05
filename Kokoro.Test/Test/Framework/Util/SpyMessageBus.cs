2ee18e64f34aea9e2c8ae3b016d0399f106636a0
netaneru <netaneru3@gmail.com>
2022-03-06 03:05:12 +0800

    Ensure error message correctness

---
+++ Kokoro.Test/Test/Framework/Util/SpyMessageBus.cs
@@ -0,0 +1,39 @@
+﻿namespace Kokoro.Test.Framework.Util;
+using System;
+using System.Collections.Generic;
+
+// Modified from, https://github.com/xunit/xunit/blob/2.4.1/test/test.utility/TestDoubles/SpyMessageBus.cs
+public class SpyMessageBus : LongLivedMarshalByRefObject, Xunit.Sdk.IMessageBus {
+	private readonly Func<IMessageSinkMessage, bool> _CancellationThunk;
+	private List<IMessageSinkMessage>? _Messages = new();
+
+	public SpyMessageBus(Func<IMessageSinkMessage, bool>? cancellationThunk = null) {
+		_CancellationThunk = cancellationThunk ?? (msg => true);
+	}
+
+	public List<IMessageSinkMessage> Messages => _Messages
+		?? throw new ObjectDisposedException(nameof(SpyMessageBus));
+
+	/// <inheritdoc/>
+	public virtual void Dispose() {
+		var msgs = _Messages;
+		if (msgs is null) {
+			return; // Already disposed
+		}
+
+		foreach (var msg in msgs) {
+			if (msg is IDisposable disposable) {
+				disposable.Dispose();
+			}
+		}
+
+		_Messages = null; // Mark as disposed
+		GC.SuppressFinalize(this);
+	}
+
+	/// <inheritdoc/>
+	public bool QueueMessage(IMessageSinkMessage message) {
+		Messages.Add(message);
+		return _CancellationThunk(message);
+	}
+}
---
+++ Kokoro.Test/Test/SelfTest/Framework/Discovery/TestFrameworkDiscoverer_Test.cs
@@ -0,0 +1,71 @@
+﻿namespace Kokoro.Test.SelfTest.Framework.Discovery;
+using Kokoro.Test.Framework.Util;
+using Moq;
+using Xunit.Abstractions;
+using Xunit.Sdk;
+
+public class TestFrameworkDiscoverer_Test {
+
+	[Fact(DisplayName = $"{nameof(T001)} :: " +
+		$"Error message is still correct about `{nameof(ITestFactAttribute)}` " +
+		$"being currently implemented by both `{nameof(TestFactAttribute)}` " +
+		$"and `{nameof(TestTheoryAttribute)}`")]
+	public void T001() {
+		using var scope = new AssertionScope();
+
+		var assemblyInfo = new ReflectionAssemblyInfo(typeof(TestFrameworkDiscoverer).Assembly);
+		using var spyBus = new SpyMessageBus();
+		using var testFrameworkDiscoverer = new TestFrameworkDiscoverer(
+			assemblyInfo, new NullSourceInformationProvider(), new NullMessageSink());
+
+		var mockRepo = new MockRepository(MockBehavior.Strict);
+		var testMethod = mockRepo.OneOf<ITestMethod>(tm =>
+			tm.Method.GetCustomAttributes(typeof(FactAttribute).AssemblyQualifiedName) == new[] {
+				Mock.Of<IReflectionAttributeInfo>(r => r.Attribute == mockRepo.OneOf<FactAttribute>())
+			} &&
+			tm.Method.GetCustomAttributes(typeof(LabelAttribute).AssemblyQualifiedName) == new[] {
+				Mock.Of<IReflectionAttributeInfo>(r => r.Attribute == mockRepo.OneOf<LabelAttribute>())
+			} &&
+			tm.TestClass.Class.Name == $"Sample_Test" &&
+			tm.Method.Name == $"Test" &&
+			tm.TestClass.TestCollection.TestAssembly.Assembly == assemblyInfo
+		);
+
+		testFrameworkDiscoverer.FindTestsForMethod(
+			testMethod, includeSourceInformation: false,
+			spyBus, TestFrameworkOptions.ForDiscovery()
+		);
+		var errorTestCase = (ExecutionErrorTestCase)spyBus.Messages
+			.OfType<ITestCaseDiscoveryMessage>()
+			.Select(msg => msg.TestCase)
+			.Single();
+
+		// Assert that the error message still says that `TestFactAttribute`
+		// and `TestTheoryAttribute` both implements `ITestFactAttribute`
+		errorTestCase.ErrorMessage.Should().MatchEquivalentOf(
+			$"*whose [Fact]-derived attribute implements ?{nameof(ITestFactAttribute)}?" +
+			$" (e.g., ?{nameof(TestFactAttribute)}? or ?{nameof(TestTheoryAttribute)}?)"
+		);
+
+		// Assert that the error message above is correct about `TestFactAttribute`
+		// and `TestTheoryAttribute` both implementing `ITestFactAttribute`
+		typeof(TestFactAttribute).Should().Implement(typeof(ITestFactAttribute));
+		typeof(TestTheoryAttribute).Should().Implement(typeof(ITestFactAttribute));
+	}
+
+	// --
+
+	// Exposes `protected` members that we want to test
+	private class TestFrameworkDiscoverer : Test.Framework.Discovery.TestFrameworkDiscoverer {
+
+		public TestFrameworkDiscoverer(
+			IAssemblyInfo assemblyInfo,
+			ISourceInformationProvider sourceProvider,
+			IMessageSink diagnosticMessageSink,
+			IXunitTestCollectionFactory? collectionFactory = null
+		) : base(assemblyInfo, sourceProvider, diagnosticMessageSink, collectionFactory) { }
+
+		public new bool FindTestsForMethod(ITestMethod testMethod, bool includeSourceInformation, IMessageBus messageBus, ITestFrameworkDiscoveryOptions discoveryOptions)
+			=> base.FindTestsForMethod(testMethod, includeSourceInformation, messageBus, discoveryOptions);
+	}
+}
