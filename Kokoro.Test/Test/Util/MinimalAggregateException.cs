08c8204ac2ba4fe01862139710fe9d3e113966a6
netaneru <netaneru3@gmail.com>
2022-03-07 05:34:34 +0800

    An `AggregateException` with an empty message
    …unlike the original `AggregateException`, which unnecessarily repeats
    all inner exception messages.

---
+++ Kokoro.Test/Test/Util/MinimalAggregateException.cs
@@ -0,0 +1,17 @@
+﻿namespace Kokoro.Test.Util;
+using System.Runtime.Serialization;
+
+public class MinimalAggregateException : AggregateException {
+
+	public MinimalAggregateException() : base("") { }
+
+	public MinimalAggregateException(IEnumerable<Exception> innerExceptions) : base("", innerExceptions) { }
+
+	public MinimalAggregateException(params Exception[] innerExceptions) : base("", innerExceptions) { }
+
+	public MinimalAggregateException(Exception innerException) : base("", innerException) { }
+
+	protected MinimalAggregateException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+
+	public override string Message => "";
+}
