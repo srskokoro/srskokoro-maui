5069bea6100a19517fcd3d5b24e147c85ddc3d46
netaneru <netaneru3@gmail.com>
2022-03-10 16:25:30 +0800

    More tests

---
+++ Kokoro.Test/Test/SelfTest/Util/ComprehensiveAssert_Facts.cs
@@ -0,0 +1,66 @@
+﻿namespace Kokoro.Test.SelfTest.Util;
+
+using Xunit.Sdk;
+
+public class ComprehensiveAssert_Facts {
+
+	[TestFact]
+	[TLabel($"[m] is implemented properly")]
+	public void T001_ProperlyImplements_Equals() {
+		Guid guid1 = Guid.NewGuid();
+		Guid guid2 = Guid.NewGuid();
+
+		Skip.If(guid1 == guid2, "Rare event: 2 values returned from `Guid.NewGuid()` are apparently equal!");
+
+		object box1 = guid1;
+		object box2 = guid1;
+		object box3 = guid1;
+		object box4 = guid2;
+
+		using (new AssertionCapture()) {
+			// First, make sure our assumptions are correct
+			box1.Should().NotBeSameAs(box2).And.NotBeSameAs(box3);
+			box2.Should().NotBeSameAs(box3);
+		}
+
+		// --
+
+		using var scope = new AssertionCapture();
+
+		new Action(() => {
+			ComprehensiveAssert.ProperlyImplements_Equals(box1, box2, box3, box4);
+		}).Should().NotThrow();
+
+		new Action(() => {
+			ComprehensiveAssert.ProperlyImplements_Equals<object>(1, 2, 3, 1);
+		}).Should().Throw<XunitException>();
+	}
+
+	[TestFact]
+	[TLabel($"[m] is implemented properly")]
+	public void T002_ProperlyImplements_GetHashCode() {
+		Guid guid1 = Guid.NewGuid();
+
+		object box1 = guid1;
+		object box2 = guid1;
+		object box3 = guid1;
+
+		using (new AssertionCapture()) {
+			// First, make sure our assumptions are correct
+			box1.Should().NotBeSameAs(box2).And.NotBeSameAs(box3);
+			box2.Should().NotBeSameAs(box3);
+		}
+
+		// --
+
+		using var scope = new AssertionCapture();
+
+		new Action(() => {
+			ComprehensiveAssert.ProperlyImplements_GetHashCode(box1, box2, box3);
+		}).Should().NotThrow();
+
+		new Action(() => {
+			ComprehensiveAssert.ProperlyImplements_GetHashCode<object>(1, 2, 3);
+		}).Should().Throw<XunitException>();
+	}
+}
---
+++ Kokoro.Test/Test/SelfTest/Util/RaceTest_Facts.cs
@@ -0,0 +1,71 @@
+﻿namespace Kokoro.Test.SelfTest.Util;
+
+using Xunit.Sdk;
+
+public class RaceTest_Facts {
+
+	[TestFact]
+	[TLabel($"Fails on simple 'check-then-act' race condition")]
+	public void T001() {
+		new Action(() => {
+			int barrier = 0;
+			int entered = 0;
+			using RaceTest race = new();
+			race.Queue(-1, 0x100, () => {
+				if (barrier != 0) {
+					return;
+				}
+				barrier = 1;
+
+				// Only 1 thread should be here at this point.
+				TestUtil.CheckEntry(ref entered).Should().Be(1);
+
+				barrier = 0;
+			});
+		}).Should().Throw<XunitException>();
+	}
+
+	[TestFact]
+	[TLabel($"Succeeds on properly implemented 'check-then-act' operation")]
+	public void T002() {
+		new Action(() => {
+			int barrier = 0;
+			int entered = 0;
+			using RaceTest race = new();
+			race.Queue(-1, 0x100, () => {
+				if (Interlocked.CompareExchange(ref barrier, 1, 0) != 0) {
+					return;
+				}
+
+				// Only 1 thread should be here at this point.
+				TestUtil.CheckEntry(ref entered).Should().Be(1);
+
+				barrier = 0;
+			});
+		}).Should().NotThrow();
+	}
+
+	[TestFact]
+	[TLabel($"Fails quickly after a thread throws")]
+	public void T003() {
+		using var scope = new AssertionCapture();
+
+		const int RunsPerSpins = 0x100;
+		int NonThrowingThreads = Environment.ProcessorCount;
+		int i = 0;
+
+		new Action(() => {
+			using RaceTest race = new(-1, 2000);
+			race.Queue(1 + NonThrowingThreads, RunsPerSpins, () => {
+				if (Interlocked.Increment(ref i) == 1) {
+					throw new Exception();
+				} else {
+					Thread.Sleep(0);
+				}
+			});
+		}).Should().Throw<Exception>();
+
+		int ExpectedIncrements = RunsPerSpins * NonThrowingThreads + 1;
+		i.Should().BeLessThanOrEqualTo(ExpectedIncrements);
+	}
+}
