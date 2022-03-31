a490de6fbb67c912498be22a9c9a4d9102882504
netaneru <netaneru3@gmail.com>
2022-03-31 21:19:14 +0800

    Future maintenance mechanism

---
+++ src/Kokoro/IProgressLite.cs
@@ -0,0 +1,9 @@
+﻿namespace Kokoro;
+
+/// <summary>
+/// A common interface for implementations of lightweight progress reporting
+/// that doesn't actually report progress. A UI should instead periodically
+/// check the structure for changes in progress.
+/// </summary>
+/// <seealso cref="IProgress{T}"/>
+public interface IProgressLite { }
---
+++ src/Kokoro/KokoroCollectionMaintenance.cs
@@ -0,0 +1,58 @@
+﻿namespace Kokoro;
+
+// This is a `ref struct` to ensure that it won't escape the current stack, so
+// that only the current thread can only ever use this object.
+//
+// This is important because this object can perform operations that can
+// corrupt the collection if accessed concurrently across multiple threads. By
+// making this a `ref struct`, there won't be any room for misusage, since
+// simply advising to avoid concurrent access doesn't necessarily mean that
+// accidental misuse is impossible (with the consequences being too harmful to
+// allow such accidents to happen).
+//
+// Another benefit is that, we will only have to mark the collection once for
+// exclusive usage and multiple operations can be performed afterwards.
+public readonly ref struct KokoroCollectionMaintenance {
+	// Only really used when we need to access some collection entities.
+	// Otherwise, it's merely used to take exclusive usage of the collection.
+	private readonly KokoroCollectionExclusive _Collection;
+
+	public KokoroCollectionMaintenance(KokoroContext context)
+		=> _Collection = new KokoroCollectionExclusive(context);
+
+	public void Dispose() => _Collection.Dispose();
+
+	// --
+
+	public class CompressRowIdsProgress : IProgressLite {
+		private volatile string _TableBeingProcessed = "";
+		private long _NumRowIdsInTheTable;
+		private long _NumRowIdsProcessedSoFar;
+
+		public string TableBeingProcessed {
+			get => _TableBeingProcessed;
+			set => _TableBeingProcessed = value;
+		}
+
+		public long NumRowIdsInTheTable {
+			// NOTE: As per the reference source, `Volatile` guarantees atomicity for `long`
+			get => Volatile.Read(ref _NumRowIdsInTheTable);
+			set => Volatile.Write(ref _NumRowIdsInTheTable, value);
+		}
+
+		public long NumRowIdsProcessedSoFar {
+			// NOTE: As per the reference source, `Volatile` guarantees atomicity for `long`
+			get => Volatile.Read(ref _NumRowIdsProcessedSoFar);
+			set => Volatile.Write(ref _NumRowIdsProcessedSoFar, value);
+		}
+	}
+
+	/// <summary>
+	/// Runs a long running operation that removes rowid gaps and ensures that
+	/// the first rowid is 1. Zero and negative rowids are left untouched.
+	/// </summary>
+	public void CompressRowIds(CompressRowIdsProgress progress) {
+		// TODO Implement
+		throw new NotImplementedException("TODO");
+	}
+}
