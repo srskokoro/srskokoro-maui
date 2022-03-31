39c5fd2e96518a6f3880485c5b920b3078b7d66f
netaneru <netaneru3@gmail.com>
2022-03-31 20:45:57 +0800

    Can be `Span<byte>` instead

--- src/Kokoro.Common/IO/IOUtils.cs
+++ src/Kokoro.Common/IO/IOUtils.cs
@@ -14,7 +14,7 @@ internal static partial class IOUtils {
 	public static byte[] ToUTF8Bytes(this string s) => Encoding.UTF8.GetBytes(s);
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	public static ReadOnlySpan<byte> ToUTF8Bytes(this string s, Span<byte> bytes) => bytes[..s.GetUTF8Bytes(bytes)];
+	public static Span<byte> ToUTF8Bytes(this string s, Span<byte> bytes) => bytes[..s.GetUTF8Bytes(bytes)];
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static int GetUTF8Bytes(this string s, Span<byte> bytes) => Encoding.UTF8.GetBytes(s, bytes);
