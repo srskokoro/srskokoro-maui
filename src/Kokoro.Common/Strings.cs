81ccc01d199ed4f57f9c1fcf9fc8fbf1db63beda
netaneru <netaneru3@gmail.com>
2022-04-01 20:43:34 +0800

    Just "unlikely"
    Since, future versions of the framework may still change the memory
    layout for strings. And we'll have to watch for that.

--- src/Kokoro.Common/Strings.cs
+++ src/Kokoro.Common/Strings.cs
@@ -11,8 +11,8 @@ internal static class Strings {
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static ref char UnsafeCreate(int length, out string result) {
 		// Given how strings are represented in memory, and that we're creating
-		// via `new string()`, it's very unlikely that the trick below will
-		// break something. That is, it should be guaranteed that we'll get a
+		// via `new string()`, it's unlikely that the trick below will break
+		// something. That is, it should be guaranteed that we'll get a
 		// different reference (or `ref char` location) every time.
 		result = new('\0', length);
 		return ref MemoryMarshal.GetReference<char>(result);
--- src/Kokoro/UniqueId.cs
+++ src/Kokoro/UniqueId.cs
@@ -475,8 +475,8 @@ public readonly struct UniqueId : IEquatable<UniqueId>, IComparable, IComparable
 		string str = new('\0', _Base58Size);
 		UnsafeWriteBase58Chars(MemoryMarshal.CreateSpan(ref MemoryMarshal.GetReference(str.AsSpan()), _Base58Size));
 		// Given how strings are represented in memory, and that we're creating
-		// via `new string()`, it's very unlikely that the above trick will
-		// break something. That is, it should be guaranteed that we'll get a
+		// via `new string()`, it's unlikely that the above trick will break
+		// something. That is, it should be guaranteed that we'll get a
 		// different reference (or `ref char` location) every time.
 		return str;
 	}
