0fb64b3f10a71442bf36a5f305b7f159cd19060e
netaneru <netaneru3@gmail.com>
2022-04-02 23:42:08 +0800

    Removed: consider using "Clockwork Base32" with `UniqueId` instead

--- src/Kokoro/UniqueId.cs
+++ src/Kokoro/UniqueId.cs
@@ -623,6 +623,15 @@ public readonly struct UniqueId : IEquatable<UniqueId>, IComparable, IComparable
 
 	#endregion
 
+	#region Clockwork Base32 Conversions
+
+	// Clockwork Base32 is a simple variant of Base32 inspired by Crockford's Base32.
+	// - Official spec at, https://gist.github.com/szktty/228f85794e4187882a77734c89c384a8
+
+	// TODO Implement. Needed for creating unique filenames.
+
+	#endregion
+
 	#region Equality and Comparability
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
