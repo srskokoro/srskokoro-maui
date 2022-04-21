5e06be0e3225d8303e3f4088213bb33211666701
netaneru <netaneru3@gmail.com>
2022-04-22 02:01:22 +0800

    Specialized facilities for nesting transactions (for internal use)

---
+++ src/Kokoro/Internal/Sqlite/NestingWriteTransaction.cs
@@ -0,0 +1,60 @@
+﻿namespace Kokoro.Internal.Sqlite;
+using static SQLitePCL.raw;
+
+internal ref struct NestingWriteTransaction {
+	private KokoroSqliteDb? _Db;
+	private readonly int _IsOutermost;
+
+	public NestingWriteTransaction(KokoroSqliteDb db) {
+		string cmd;
+		// NOTE: Auto-commit is set (non-zero) while no transaction is active.
+		if ((_IsOutermost = sqlite3_get_autocommit(db.Handle)) != 0) {
+			cmd = "BEGIN IMMEDIATE";
+		} else {
+			cmd = "SAVEPOINT _";
+			// ^ NOTE: The SQLite docs explicitly say that "transaction names
+			// need not be unique" and that only the *most recent* savepoint
+			// with a matching name is released or rolled back; in other words,
+			// we're allowed to use the same savepoint name even if there's
+			// already an outer savepoint using the same name. It also means
+			// that we can use any savepoint name with practically nothing to
+			// worry about. See, https://www.sqlite.org/lang_savepoint.html
+		}
+
+		// TODO Use (and reuse) `sqlite3_stmt` directly with `SQLITE_PREPARE_PERSISTENT`
+		db.Exec(cmd);
+		_Db = db;
+	}
+
+	public void Commit() {
+		string cmd = _IsOutermost != 0 ? "END" : "RELEASE _";
+		try {
+			// TODO Use (and reuse) `sqlite3_stmt` directly with `SQLITE_PREPARE_PERSISTENT`
+			_Db!.Exec(cmd);
+		} catch (NullReferenceException) when (_Db == null) {
+			throw Ex_AlreadyReleased_InvOp();
+		}
+		_Db = null;
+	}
+
+	public void Dispose() {
+		var db = _Db;
+		if (db == null) return;
+
+		// Make sure that the current transaction is still active and wasn't
+		// rolled back automatically due to an error.
+		//
+		// See, https://www.sqlite.org/lang_transaction.html#response_to_errors_within_a_transaction:~:text=Response%20To%20Errors%20Within%20A%20Transaction
+		//
+		// NOTE: Auto-commit is unset (zero) while a transaction is active.
+		if (sqlite3_get_autocommit(db.Handle) == 0) {
+			// TODO Use (and reuse) `sqlite3_stmt` directly with `SQLITE_PREPARE_PERSISTENT`
+			db.Exec(_IsOutermost != 0 ? "ROLLBACK" : "ROLLBACK TO _; RELEASE _");
+		}
+		_Db = null;
+	}
+
+	[MethodImpl(MethodImplOptions.NoInlining)]
+	private static InvalidOperationException Ex_AlreadyReleased_InvOp()
+		=> new($"Transaction already complete; it's no longer usable.");
+}
---
+++ src/Kokoro/Internal/Sqlite/OptionalReadTransaction.cs
@@ -0,0 +1,42 @@
+﻿namespace Kokoro.Internal.Sqlite;
+using static SQLitePCL.raw;
+
+internal ref struct OptionalReadTransaction {
+	private KokoroSqliteDb? _Db;
+
+	public OptionalReadTransaction(KokoroSqliteDb db) {
+		// NOTE: Auto-commit is set (non-zero) while no transaction is active.
+		if (sqlite3_get_autocommit(db.Handle) != 0) {
+			// TODO Use (and reuse) `sqlite3_stmt` directly with `SQLITE_PREPARE_PERSISTENT`
+			db.Exec("BEGIN");
+			_Db = db;
+		} else {
+			_Db = null;
+		}
+	}
+
+	public void Dispose() {
+		var db = _Db;
+		if (db != null) {
+			// Make sure that the current transaction is still active and wasn't
+			// rolled back automatically due to an error.
+			//
+			// See, https://www.sqlite.org/lang_transaction.html#response_to_errors_within_a_transaction:~:text=Response%20To%20Errors%20Within%20A%20Transaction
+			//
+			// NOTE: Auto-commit is unset (zero) while a transaction is active.
+			if (sqlite3_get_autocommit(db.Handle) == 0) {
+				// TODO Use (and reuse) `sqlite3_stmt` directly with `SQLITE_PREPARE_PERSISTENT`
+				// - If the above TODO is done, then we won't even need to check
+				// for the auto-commit flag. According to the SQLite docs (in
+				// the link below), even if a ROLLBACK fails (because there's no
+				// active transaction), no harm will be done.
+				//   - https://www.sqlite.org/lang_transaction.html#response_to_errors_within_a_transaction:~:text=Response%20To%20Errors%20Within%20A%20Transaction
+				//   - In fact, the SQLite docs even recommends rolling back
+				//   when certain kinds of errors occur, even if such errors
+				//   may already have rolled back the transaction automatically.
+				db.Exec("ROLLBACK");
+			}
+			_Db = null;
+		}
+	}
+}
