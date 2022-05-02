6080165aee3819bf6bd95540aab33a3ba7937ef7
netaneru <netaneru3@gmail.com>
2022-05-03 04:49:49 +0800

    Clarity

--- src/Kokoro.Common/Pooling/DisposingObjectPool.cs
+++ src/Kokoro.Common/Pooling/DisposingObjectPool.cs
@@ -158,6 +158,8 @@ internal class DisposingObjectPool<T> : ObjectPool<T>, IDisposable where T : IDi
 		// Re-throw any pending exception, especially `ThreadInterruptedException`
 		if (exc != null) {
 			Exception? ex = exc.ConsolidateException();
+			// Let callers catch `ThreadInterruptedException` in a
+			// straightforward manner.
 			if (interrupted && ex is not ThreadInterruptedException) {
 				ex = new ThreadInterruptedException(null, ex);
 			}
