c9c9e268cbe6bc24794f057f83fc8bb34f187801
netaneru <netaneru3@gmail.com>
2022-05-04 07:30:38 +0800

    Easily get a `UniqueId` from an `SqliteDataReader`

---
+++ src/Kokoro/Internal/Sqlite/Extensions/SqliteDataReaderExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace Kokoro.Internal.Sqlite.Extensions;
+using Microsoft.Data.Sqlite;
+
+internal static class SqliteDataReaderExtensions {
+
+	public static UniqueId GetUniqueId(this SqliteDataReader reader, int ordinal) {
+		using var s = reader.GetStream(ordinal);
+		return s.Read<UniqueId>();
+	}
+}
