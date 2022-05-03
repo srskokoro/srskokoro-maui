49d7ce7fa44134f944f4e53aea166a4b1e24bd17
netaneru <netaneru3@gmail.com>
2022-05-04 04:22:25 +0800

    Easily assert SQLite column names on DEBUG

---
+++ src/Kokoro.Common/Sqlite/Extensions/SqliteDataReaderExtensions.cs
@@ -0,0 +1,11 @@
+﻿namespace Kokoro.Common.Sqlite.Extensions;
+using Microsoft.Data.Sqlite;
+
+internal static class SqliteDataReaderExtensions {
+
+	[Conditional("DEBUG")]
+	public static void DebugAssert_Name(this SqliteDataReader reader, int ordinal, string name) {
+		string actualName = reader.GetName(ordinal);
+		Debug.Assert(actualName == name, $"Column {ordinal}; Expected name: {name}; Actual name: {actualName}");
+	}
+}
