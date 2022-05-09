2d9e8945cdbada100fd9795faeacf4fb1d3004e2
netaneru <netaneru3@gmail.com>
2022-05-09 12:03:38 +0800

    Always prefer the shorter name instead

--- src/Kokoro.Common/Sqlite/Extensions/SqliteDataReaderExtensions.cs
+++ src/Kokoro.Common/Sqlite/Extensions/SqliteDataReaderExtensions.cs
@@ -4,13 +4,8 @@ using Microsoft.Data.Sqlite;
 internal static class SqliteDataReaderExtensions {
 
 	[Conditional("DEBUG")]
-	public static void DebugAssert_Name(this SqliteDataReader reader, int ordinal, string name) {
+	public static void DAssert_Name(this SqliteDataReader reader, int ordinal, string name) {
 		string actualName = reader.GetName(ordinal);
 		Debug.Assert(actualName == name, $"Column {ordinal}; Expected name: {name}; Actual name: {actualName}");
 	}
-
-	[MethodImpl(MethodImplOptions.AggressiveInlining)]
-	[Conditional("DEBUG")]
-	public static void DAssert_Name(this SqliteDataReader reader, int ordinal, string name)
-		=> reader.DebugAssert_Name(ordinal, name);
 }
