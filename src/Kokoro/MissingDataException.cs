37623fb0fbbd6eae3682d726a65afd4578eaa17e
netaneru <netaneru3@gmail.com>
2022-05-10 18:15:21 +0800

    Utility exceptions for expected data not found

---
+++ src/Kokoro/MissingDataException.cs
@@ -0,0 +1,18 @@
+﻿namespace Kokoro;
+using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class MissingDataException : InvalidOperationException {
+	private const string DefaultMessage = "Needed data missing";
+
+	public MissingDataException() : base(DefaultMessage) { }
+
+	public MissingDataException(string? message) : base(message) { }
+
+	public MissingDataException(string? message, Exception? innerException) : base(message, innerException) { }
+
+	public MissingDataException(Exception? innerException) : base(DefaultMessage, innerException) { }
+
+	protected MissingDataException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
---
+++ src/Kokoro/MissingRecordException.cs
@@ -0,0 +1,18 @@
+﻿namespace Kokoro;
+using System;
+using System.Runtime.Serialization;
+
+[Serializable]
+public class MissingRecordException : MissingDataException {
+	private const string DefaultMessage = "Needed record missing";
+
+	public MissingRecordException() : base(DefaultMessage) { }
+
+	public MissingRecordException(string? message) : base(message) { }
+
+	public MissingRecordException(string? message, Exception? innerException) : base(message, innerException) { }
+
+	public MissingRecordException(Exception? innerException) : base(DefaultMessage, innerException) { }
+
+	protected MissingRecordException(SerializationInfo info, StreamingContext context) : base(info, context) { }
+}
