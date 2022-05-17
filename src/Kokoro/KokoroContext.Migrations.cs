64d1f7aa9fa3404d231da0ae6a667509860ccb08
netaneru <netaneru3@gmail.com>
2022-05-18 04:53:14 +0800

    Easily get index

--- src/Kokoro/KokoroContext.Migrations.cs
+++ src/Kokoro/KokoroContext.Migrations.cs
@@ -223,6 +223,8 @@ partial class KokoroContext {
 
 			"index_loc INTEGER NOT NULL AS ((index_st >> 1) | (index_st & 0x1))," +
 
+			"index INTEGER NOT NULL AS (index_st >> 2)," +
+
 			// The field store type:
 			// - 0b00: Shared
 			// - 0b01: Hot
