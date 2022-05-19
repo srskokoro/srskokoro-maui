6165287b90acd9d55dbc6fa47b8fed51430a40d6
netaneru <netaneru3@gmail.com>
2022-05-20 03:42:19 +0800

    Get min bytes needed to store a `uint` or `ulong`

--- src/Kokoro.Common/Bytes.cs
+++ src/Kokoro.Common/Bytes.cs
@@ -1,4 +1,5 @@
 ﻿namespace Kokoro.Common;
+using System.Numerics;
 
 internal static class Bytes {
 
@@ -8,5 +9,29 @@ internal static class Bytes {
 		// - https://github.com/CommunityToolkit/dotnet/blob/f003c4fc6f93ff280cff5208abf8a54372556049/CommunityToolkit.HighPerformance/Extensions/BoolExtensions.cs#L21
 		byte copy = flag;
 		return *(bool*)&copy;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static int CountBytesNeeded(this uint value)
+		=> (32 + 7 - BitOperations.LeadingZeroCount(value)) >> 3;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static int CountBytesNeeded(this ulong value)
+		=> (64 + 7 - BitOperations.LeadingZeroCount(value)) >> 3;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static int CountBytesNeededOr1(this uint value) {
+		int x = value.CountBytesNeeded();
+		// NOTE: On ARM and Intel machines, the following generates less asm
+		// than the equivalent `x | (x == 0)`
+		return x | (int)((uint)(x-1) >> 31);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static int CountBytesNeededOr1(this ulong value) {
+		int x = value.CountBytesNeeded();
+		// NOTE: On ARM and Intel machines, the following generates less asm
+		// than the equivalent `x | (x == 0)`
+		return x | (int)((uint)(x-1) >> 63);
 	}
 }
