9311263c11a26f2b3ffab26e44968e5a396c997e
netaneru <netaneru3@gmail.com>
2022-05-20 00:42:45 +0800

    Much better exception to keep things consistent

--- src/Kokoro/Internal/Marshal/Fields/BaseFieldsReader.cs
+++ src/Kokoro/Internal/Marshal/Fields/BaseFieldsReader.cs
@@ -163,7 +163,8 @@ internal abstract partial class BaseFieldsReader<TOwner> : FieldsReader
 
 	public override int ModStampCount => 0;
 
-	public override long ReadModStamp(int index) => throw new NotSupportedException();
+	public override long ReadModStamp(int index)
+		=> throw new ArgumentOutOfRangeException(nameof(index));
 
 	public override void Dispose() => _Stream.Dispose();
 }
