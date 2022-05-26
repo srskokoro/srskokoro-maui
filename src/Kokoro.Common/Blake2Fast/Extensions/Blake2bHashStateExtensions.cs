5e11b10346cb52433f685f8bd3e25a6de2c5c7e7
netaneru <netaneru3@gmail.com>
2022-05-27 05:08:11 +0800

    Easily control endianness of input

--- src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions.cs
+++ src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions.cs
@@ -1,8 +1,9 @@
 ﻿namespace Kokoro.Common.Blake2Fast.Extensions;
 using global::Blake2Fast.Implementation;
 using Kokoro.Common.Util;
+using System.Buffers.Binary;
 
-internal static class Blake2bHashStateExtensions {
+internal static partial class Blake2bHashStateExtensions {
 
 	public static void UpdateWithVarInt(this Blake2bHashState state, uint input) {
 		Span<byte> buffer = stackalloc byte[5];
@@ -14,5 +15,211 @@ internal static class Blake2bHashStateExtensions {
 		Span<byte> buffer = stackalloc byte[9];
 		VarInts.Write(buffer, input);
 		state.Update(buffer);
+	}
+
+	// --
+
+	// Exists only for completeness
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, byte input) => state.Update(input);
+
+	// Exists only for completeness
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, byte input) => state.Update(input);
+}
+
+// Little-Endian
+static partial class Blake2bHashStateExtensions {
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, short input) {
+		if (!BitConverter.IsLittleEndian) {
+			short tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, int input) {
+		if (!BitConverter.IsLittleEndian) {
+			int tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, long input) {
+		if (!BitConverter.IsLittleEndian) {
+			long tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	// --
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, ushort input) {
+		if (!BitConverter.IsLittleEndian) {
+			ushort tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, uint input) {
+		if (!BitConverter.IsLittleEndian) {
+			uint tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, ulong input) {
+		if (!BitConverter.IsLittleEndian) {
+			ulong tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	// --
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, Half input) {
+		if (!BitConverter.IsLittleEndian) {
+			ushort tmp = BinaryPrimitives.ReverseEndianness(BitConverter.HalfToUInt16Bits(input));
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, float input) {
+		if (!BitConverter.IsLittleEndian) {
+			uint tmp = BinaryPrimitives.ReverseEndianness(BitConverter.SingleToUInt32Bits(input));
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateLE(this Blake2bHashState state, double input) {
+		if (!BitConverter.IsLittleEndian) {
+			ulong tmp = BinaryPrimitives.ReverseEndianness(BitConverter.DoubleToUInt64Bits(input));
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+}
+
+// Big-Endian
+static partial class Blake2bHashStateExtensions {
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, short input) {
+		if (BitConverter.IsLittleEndian) {
+			short tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, int input) {
+		if (BitConverter.IsLittleEndian) {
+			int tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, long input) {
+		if (BitConverter.IsLittleEndian) {
+			long tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	// --
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, ushort input) {
+		if (BitConverter.IsLittleEndian) {
+			ushort tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, uint input) {
+		if (BitConverter.IsLittleEndian) {
+			uint tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, ulong input) {
+		if (BitConverter.IsLittleEndian) {
+			ulong tmp = BinaryPrimitives.ReverseEndianness(input);
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	// --
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, Half input) {
+		if (BitConverter.IsLittleEndian) {
+			ushort tmp = BinaryPrimitives.ReverseEndianness(BitConverter.HalfToUInt16Bits(input));
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, float input) {
+		if (BitConverter.IsLittleEndian) {
+			uint tmp = BinaryPrimitives.ReverseEndianness(BitConverter.SingleToUInt32Bits(input));
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static void UpdateBE(this Blake2bHashState state, double input) {
+		if (BitConverter.IsLittleEndian) {
+			ulong tmp = BinaryPrimitives.ReverseEndianness(BitConverter.DoubleToUInt64Bits(input));
+			state.Update(tmp);
+		} else {
+			state.Update(input);
+		}
 	}
 }
