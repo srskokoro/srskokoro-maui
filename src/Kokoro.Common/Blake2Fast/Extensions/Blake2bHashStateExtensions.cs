d3f1e41b1e5692285d51aed07d0e30ddf7e6efe9
netaneru <netaneru3@gmail.com>
2022-05-27 05:04:51 +0800

    Easily feed a varint to a Blake2b hasher

---
+++ src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Kokoro.Common.Blake2Fast.Extensions;
+using global::Blake2Fast.Implementation;
+using Kokoro.Common.Util;
+
+internal static class Blake2bHashStateExtensions {
+
+	public static void UpdateWithVarInt(this Blake2bHashState state, uint input) {
+		Span<byte> buffer = stackalloc byte[5];
+		VarInts.Write(buffer, input);
+		state.Update(buffer);
+	}
+
+	public static void UpdateWithVarInt(this Blake2bHashState state, ulong input) {
+		Span<byte> buffer = stackalloc byte[9];
+		VarInts.Write(buffer, input);
+		state.Update(buffer);
+	}
+}
