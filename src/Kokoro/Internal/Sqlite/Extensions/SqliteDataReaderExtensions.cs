551ab8dee02e99daaa73c6d63bd3a761b87c7e57
netaneru <netaneru3@gmail.com>
2022-06-01 01:20:17 +0800

    Easily and optimally get blob as byte array

--- src/Kokoro/Internal/Sqlite/Extensions/SqliteDataReaderExtensions.cs
+++ src/Kokoro/Internal/Sqlite/Extensions/SqliteDataReaderExtensions.cs
@@ -7,4 +7,11 @@ internal static class SqliteDataReaderExtensions {
 		using var s = reader.GetStream(ordinal);
 		return s.Read<UniqueId>();
 	}
+
+	public static byte[] GetBytes(this SqliteDataReader reader, int ordinal) {
+		if (reader.GetValue(ordinal) is byte[] bytes) {
+			return bytes;
+		}
+		return Array.Empty<byte>();
+	}
 }
