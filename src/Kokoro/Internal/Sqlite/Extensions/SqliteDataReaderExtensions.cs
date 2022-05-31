2617a969dae9f65f24c4f3e0d47b9005054f1890
netaneru <netaneru3@gmail.com>
2022-06-01 02:00:47 +0800

    An alternative that throws on null
    - Uses the same error message as,
    https://github.com/dotnet/efcore/blob/0257e5c118d78ce1b4d5e514779240c929c4b8aa/src/Microsoft.Data.Sqlite.Core/Properties/Resources.resx#L193

--- src/Kokoro/Internal/Sqlite/Extensions/SqliteDataReaderExtensions.cs
+++ src/Kokoro/Internal/Sqlite/Extensions/SqliteDataReaderExtensions.cs
@@ -3,9 +3,22 @@ using Microsoft.Data.Sqlite;
 
 internal static class SqliteDataReaderExtensions {
 
+	[DoesNotReturn]
+	private static T E_CalledOnNullValue_InvOp<T>(int ordinal) {
+		// From, https://github.com/dotnet/efcore/blob/v6.0.3/src/Microsoft.Data.Sqlite.Core/Properties/Resources.resx#L193
+		throw new InvalidOperationException($"The data is NULL at ordinal {ordinal}. This method can't be called on NULL values. Check using IsDBNull before calling.");
+	}
+
 	public static UniqueId GetUniqueId(this SqliteDataReader reader, int ordinal) {
 		using var s = reader.GetStream(ordinal);
 		return s.Read<UniqueId>();
+	}
+
+	public static byte[]? GetBytes(this SqliteDataReader reader, int ordinal) {
+		if (reader.GetValue(ordinal) is byte[] bytes) {
+			return bytes;
+		}
+		return E_CalledOnNullValue_InvOp<byte[]>(ordinal);
 	}
 
 	public static byte[]? GetBytesOrNull(this SqliteDataReader reader, int ordinal) {
