fbfe569eb22c709e695f9aac2cb432c500df3af4
netaneru <netaneru3@gmail.com>
2022-06-10 13:23:45 +0800

    Reserved for integer-with-floating-point hybrid as a custom format
    - The idea is to have an arbitrary-precision twos-complement integer (in
    big-endian) as the significand, prefixed by a length to indicate how
    many bytes represent the integer part of the number. The length part is
    either provided as a varint (if the type hint is `NumX`) or indicated by
    the type hint.

--- src/Kokoro/FieldTypeHint.cs
+++ src/Kokoro/FieldTypeHint.cs
@@ -37,6 +37,24 @@ public enum FieldTypeHint : int {
 	//Dec64  = 0xE,
 	//Dec128 = 0xF,
 
+	// NOTE: Reserved for integer-with-floating-point hybrid as a custom format.
+	// - The idea is to have an arbitrary-precision twos-complement integer (in
+	// big-endian) as the significand, prefixed by a length to indicate how many
+	// bytes represent the integer part of the number. The length part is either
+	// provided as a varint (if the type hint is `NumX`) or indicated by the
+	// type hint.
+	/*
+	Num8     = 0x10,
+	Num16    = 0x11,
+	Num24    = 0x12,
+	Num32    = 0x13,
+	Num40    = 0x14,
+	Num48    = 0x15,
+	Num56    = 0x16,
+	Num64    = 0x17,
+	NumX     = 0x18,
+	 */
+
 	Text     = 0x57,
 	Blob     = 0x58,
 
