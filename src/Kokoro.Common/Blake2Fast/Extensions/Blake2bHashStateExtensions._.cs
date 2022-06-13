5a746292b8fcb4eb49231e70f8261981d5de1a8e
netaneru <netaneru3@gmail.com>
2022-06-14 02:48:05 +0800

    Consider aggressive inlining

--- src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions._.cs
+++ src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions._.cs
@@ -5,12 +5,14 @@ using System.Buffers.Binary;
 
 internal static partial class Blake2bHashStateExtensions {
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void UpdateWithVarInt(this Blake2bHashState state, uint input) {
 		Span<byte> buffer = stackalloc byte[5];
 		VarInts.Write(buffer, input);
 		state.Update(buffer);
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static void UpdateWithVarInt(this Blake2bHashState state, ulong input) {
 		Span<byte> buffer = stackalloc byte[9];
 		VarInts.Write(buffer, input);
