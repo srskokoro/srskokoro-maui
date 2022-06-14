570f3e8518489e9b36fceab6241b7b7c788cdcb5
netaneru <netaneru3@gmail.com>
2022-06-14 14:50:41 +0800

    Convenience

--- src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions._.cs
+++ src/Kokoro.Common/Blake2Fast/Extensions/Blake2bHashStateExtensions._.cs
@@ -6,6 +6,15 @@ internal static partial class Blake2bHashStateExtensions {
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	[SkipLocalsInit]
+	public static Span<byte> FinishAndGet(ref this Blake2bHashState state, Span<byte> output) {
+		state.Finish(output);
+		return output;
+	}
+
+	// --
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	[SkipLocalsInit]
 	public static void UpdateWithVarInt(ref this Blake2bHashState state, uint input) {
 		Span<byte> buffer = stackalloc byte[5];
 		int written = VarInts.Write(buffer, input);
