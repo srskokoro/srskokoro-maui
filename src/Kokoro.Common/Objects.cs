f9f408952d937a67d7109b472f213178c2130d6f
netaneru <netaneru3@gmail.com>
2022-06-25 06:47:50 +0800

    Clarity

--- src/Kokoro.Common/Objects.cs
+++ src/Kokoro.Common/Objects.cs
@@ -5,6 +5,7 @@ internal static class Objects {
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	public static object OrDBNull(this object? o) {
+		// Favors the non-null case. The null case becomes the conditional jump forward.
 		if (o != null) return o;
 		return DBNull.Value;
 	}
