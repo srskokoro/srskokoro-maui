523222196fb8b4370a8146625efb4e2787e1a2db
netaneru <netaneru3@gmail.com>
2022-06-27 19:40:26 +0800

    Added 32-bit counterpart

--- src/Kokoro/Internal/IO/Extensions/StreamExtensions.Reads.cs
+++ src/Kokoro/Internal/IO/Extensions/StreamExtensions.Reads.cs
@@ -77,6 +77,29 @@ internal static partial class StreamExtensions {
 		return result;
 	}
 
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	[SkipLocalsInit]
+	public static uint ReadUIntXAsUInt32(this Stream stream, int sizeOfUIntX) {
+		const int MaxSize = sizeof(uint);
+		if ((uint)sizeOfUIntX > (uint)MaxSize) {
+			ThrowHelper.ThrowArgumentOutOfRangeException();
+		}
+
+		uint result = 0;
+		var buffer = MemoryMarshal.CreateSpan(ref U.Add(
+			ref U.As<uint, byte>(ref result), MaxSize - sizeOfUIntX), sizeOfUIntX);
+
+		int sread = stream.Read(buffer);
+		if (sread != sizeOfUIntX)
+			E_EndOfStream_InvOp();
+
+		// Needed since the `UIntX` in the stream is assumed big-endian
+		if (BitConverter.IsLittleEndian) {
+			return BinaryPrimitives.ReverseEndianness(result);
+		}
+		return result;
+	}
+
 	// --
 
 	[DoesNotReturn]
