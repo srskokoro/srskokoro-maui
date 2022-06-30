dc1f01d68acfc16aa3aae31cc88697c69faf82c6
netaneru <netaneru3@gmail.com>
2022-06-30 10:33:42 +0800

    Hassle-free shared array pool renting

---
+++ src/Kokoro.Common/Buffers/BufferRenter.cs
@@ -0,0 +1,33 @@
+﻿namespace Kokoro.Common.Buffers;
+using System.Buffers;
+
+/// <summary>
+/// A highly stripped-down version of <see cref="CommunityToolkit.HighPerformance.Buffers.SpanOwner{T}"/>
+/// that always rents from <see cref="ArrayPool{T}.Shared"/> and ensures that
+/// rented arrays containing reference types are always cleared when returning
+/// them to the pool.
+/// </summary>
+/// <typeparam name="T">The type of items to store in the current instance.</typeparam>
+internal readonly struct BufferRenter<T> : IDisposable {
+
+	public readonly T[] Array;
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public BufferRenter(int length) {
+		Array = ArrayPool<T>.Shared.Rent(length);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public BufferRenter(int length, out Span<T> span) {
+		span = (Array = ArrayPool<T>.Shared.Rent(length)).AsDangerousSpanShortened(length);
+	}
+
+	/// <inheritdoc/>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void Dispose() {
+		ArrayPool<T>.Shared.Return(
+			Array,
+			clearArray: RuntimeHelpers.IsReferenceOrContainsReferences<T>()
+		);
+	}
+}
---
+++ src/Kokoro.Common/Buffers/Extensions/ArrayExtensions.cs
@@ -0,0 +1,33 @@
+﻿namespace Kokoro.Common.Buffers.Extensions;
+using System.Runtime.InteropServices;
+
+internal static class ArrayExtensions {
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Span<T> AsDangerousSpan<T>(this T[] array)
+		=> array.AsDangerousSpanShortened(array.Length);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Span<T> AsDangerousSpan<T>(this T[] array, int start)
+		=> array.AsDangerousSpan(start, array.Length);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Span<T> AsDangerousSpan<T>(this T[] array, int start, int length) {
+		/// See note in <see cref="AsDangerousSpanShortened{T}(T[], int)"/>
+
+		ref T r0 = ref array.DangerousGetReferenceAt(start);
+		return MemoryMarshal.CreateSpan(ref r0, length);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static Span<T> AsDangerousSpanShortened<T>(this T[] array, int length) {
+		// NOTE: As of writing this, the following produces more efficient asm
+		// than `new Span<T>(array, 0, array.Length)` -- with the latter being
+		// more efficient than `new Span<T>(array)` or casting to `Span<T>`.
+		//
+		// The approach was borrowed from, https://github.com/CommunityToolkit/dotnet/blob/v8.0.0-preview3/CommunityToolkit.HighPerformance/Buffers/SpanOwner%7BT%7D.cs#L139
+
+		ref T r0 = ref array.DangerousGetReference();
+		return MemoryMarshal.CreateSpan(ref r0, length);
+	}
+}
