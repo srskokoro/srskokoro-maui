3303e50e1b93d5da75d5c8424e7e30b941b31e99
netaneru <netaneru3@gmail.com>
2022-07-09 20:03:19 +0800

    A convenient bit hack

---
+++ src/Kokoro.Common/Util/BitUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace Kokoro.Common.Util;
+
+internal static class BitUtils {
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static int PosOrNegate(this int value) {
+		return value ^ (value >> 31);
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public static long PosOrNegate(this long value) {
+		return value ^ (value >> 63);
+	}
+}
