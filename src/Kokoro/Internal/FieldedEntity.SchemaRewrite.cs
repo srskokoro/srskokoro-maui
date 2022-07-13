514ff90a97cc7b696d0e553c9756f62f9a8ea05c
netaneru <netaneru3@gmail.com>
2022-07-13 13:08:01 +0800

    Implemented the gathering of direct classes

--- src/Kokoro/Internal/FieldedEntity.SchemaRewrite.cs
+++ src/Kokoro/Internal/FieldedEntity.SchemaRewrite.cs
@@ -1,4 +1,5 @@
 namespace Kokoro.Internal;
+using Microsoft.Data.Sqlite;
 
 partial class FieldedEntity {
 	private const int MaxClassCount = byte.MaxValue + 1;
@@ -9,5 +10,41 @@ partial class FieldedEntity {
 	[MethodImpl(MethodImplOptions.AggressiveOptimization)]
 	[SkipLocalsInit]
 	private protected void RewriteSchema(ref FieldsReader fr, int hotStoreLimit, ref FieldsWriter fw) {
+		var clsSet = _AddedClasses;
+		clsSet = clsSet != null ? new(clsSet) : new();
+
+		var remClsSet = _RemovedClasses;
+		remClsSet = remClsSet != null ? new(remClsSet) : clsSet;
+
+		var db = fr.Db;
+
+		// Get the old schema's direct classes
+		using (var cmd = db.CreateCommand()) {
+			cmd.Set("SELECT cls FROM SchemaToDirectClass WHERE schema=$schema")
+				.AddParams(new("$schema", _SchemaRowId));
+
+			using var r = cmd.ExecuteReader();
+			while (r.Read()) {
+				r.DAssert_Name(0, "cls");
+				long cls = r.GetInt64(0);
+
+				if (!remClsSet.Contains(cls))
+					clsSet.Add(cls);
+			}
+		}
+
+		int dclsCount = clsSet.Count;
+		if (dclsCount > MaxClassCount) E_TooManyClasses(dclsCount);
+
+		// TODO-XXX Finish implementation
+	}
+
+	[DoesNotReturn]
+	private void E_TooManyClasses(int currentCount) {
+		Debug.Assert(currentCount > MaxClassCount);
+		throw new InvalidOperationException(
+			$"Total number of classes (currently {currentCount}) shouldn't exceed {MaxClassCount};" +
+			$"{Environment.NewLine}Entity: {GetDebugLabel()};" +
+			$"{Environment.NewLine}Schema: {_SchemaRowId};");
 	}
 }
