90f6e545e203b93d3c244544c398bff49782cb5c
netaneru <netaneru3@gmail.com>
2022-07-30 09:38:08 +0800

    Should also clearly state the rules

--- src/Kokoro/Internal/FieldedEntity.Classes.cs
+++ src/Kokoro/Internal/FieldedEntity.Classes.cs
@@ -1,9 +1,24 @@
 namespace Kokoro.Internal;
 
 partial class FieldedEntity {
+	/// <summary>
+	/// The set of classes held by the <see cref="FieldedEntity">fielded entity</see>.
+	/// </summary>
 	private Classes? _Classes;
 
 	private sealed class Classes : HashSet<long> {
+		/// <summary>
+		/// The set of classes marked as changed, a.k.a., class change set.
+		/// </summary>
+		/// <remarks>
+		/// RULES:
+		/// <br/>- If a class <b>is held</b> while marked as <b>changed</b>, the
+		/// "changed" mark should be interpreted as the class awaiting <b>addition</b>
+		/// to the new schema of the <see cref="FieldedEntity">fielded entity</see>.
+		/// <br/>- If a class <b>is not held</b> while marked as <b>changed</b>,
+		/// the "changed" mark should be interpreted as the class awaiting <b>removal</b>
+		/// from the new schema, given that it existed in the old schema.
+		/// </remarks>
 		internal ClassChanges? _Changes;
 	}
 
