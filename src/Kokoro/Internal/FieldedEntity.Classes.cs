ca5e82b8c327d364e91a7bb69b688b90d54b8dfe
netaneru <netaneru3@gmail.com>
2022-07-30 11:11:15 +0800

    Class ID loading & manipulation facilities

--- src/Kokoro/Internal/FieldedEntity.Classes.cs
+++ src/Kokoro/Internal/FieldedEntity.Classes.cs
@@ -1,4 +1,5 @@
 namespace Kokoro.Internal;
+using Kokoro.Internal.Sqlite;
 
 partial class FieldedEntity {
 	/// <summary>
@@ -23,4 +24,218 @@ partial class FieldedEntity {
 	}
 
 	private sealed class ClassChanges : HashSet<long> { }
+
+	public bool IsOfClassCached(long classRowId) {
+		var classes = _Classes;
+		if (classes != null && classes.Contains(classRowId)) {
+			return true;
+		}
+		return false;
+	}
+
+	public void AddClass(long classRowId) {
+		var classes = _Classes;
+		if (classes == null) {
+			// This becomes a conditional jump forward to not favor it
+			goto Init;
+		}
+
+		var changes = classes._Changes;
+		if (changes == null) {
+			// This becomes a conditional jump forward to not favor it
+			goto InitChanges;
+		}
+
+	Set:
+		// NOTE: Mark first as held before marking as changed. Marking the class
+		// as held may fail, and if that happens while the class is marked first
+		// as changed, the "changed" mark would be interpreted incorrectly as a
+		// "class removal" instead, which isn't what we want.
+		classes.Add(classRowId); // Marks as held
+		changes.Add(classRowId); // Marks as changed
+		return;
+
+	Init:
+		_Classes = classes = new();
+	InitChanges:
+		classes._Changes = changes = new();
+		goto Set;
+	}
+
+	public void RemoveClass(long classRowId) {
+		var classes = _Classes;
+		if (classes == null) {
+			// This becomes a conditional jump forward to not favor it
+			goto Init;
+		}
+
+		// NOTE: Unmark first as held before marking as changed. Unmarking the
+		// class as held may fail, and if that happens while the class is held
+		// and marked first as changed, the "changed" mark would be interpreted
+		// incorrectly as a "class addition" instead, which isn't what we want.
+		classes.Remove(classRowId); // Unmarks as held (if marked held before)
+
+		var changes = classes._Changes;
+		if (changes == null) {
+			// This becomes a conditional jump forward to not favor it
+			goto InitChanges;
+		}
+
+	Set:
+		changes.Add(classRowId); // Marks as changed
+		return;
+
+	Init:
+		_Classes = classes = new();
+	InitChanges:
+		classes._Changes = changes = new();
+		goto Set;
+	}
+
+	/// <seealso cref="AddClassAsLoaded(long)"/>
+	public void AddClassToCache(long classRowId) {
+		var classes = _Classes;
+		if (classes == null) {
+			// This becomes a conditional jump forward to not favor it
+			goto Init;
+		}
+
+	Set:
+		classes.Add(classRowId); // Marks as held
+		return;
+
+	Init:
+		_Classes = classes = new();
+		goto Set;
+	}
+
+	/// <summary>
+	/// Same as <see cref="UnmarkClassAsChanged(long)"/> followed by
+	/// <see cref="AddClassToCache(long)"/>.
+	/// </summary>
+	public void AddClassAsLoaded(long classRowId) {
+		var classes = _Classes;
+		if (classes == null) {
+			// This becomes a conditional jump forward to not favor it
+			goto Init;
+		}
+
+		classes._Changes?.Remove(classRowId); // Unmarks as changed
+
+	Set:
+		classes.Add(classRowId); // Marks as held
+		return;
+
+	Init:
+		_Classes = classes = new();
+		goto Set;
+	}
+
+	/// <summary>
+	/// Alias for <see cref="UnloadClassId(long)"/>
+	/// </summary>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public void RemoveClassFromCache(long classRowId)
+		=> UnloadClassId(classRowId);
+
+
+	public void UnmarkClassAsChanged(long classRowId)
+		=> _Classes?._Changes?.Remove(classRowId);
+
+	public void UnmarkClassesAsChanged() {
+		var classes = _Classes;
+		if (classes != null)
+			classes._Changes = null;
+	}
+
+
+	public void UnloadClassId(long classRowId) {
+		var classes = _Classes;
+		if (classes != null) {
+			// NOTE: Unmark first as changed before unmarking as held. Unmarking
+			// the class as changed may fail, and if that happens while the
+			// class is unmarked first as held, the "changed" mark would be
+			// interpreted incorrectly as a "class removal" -- a potentially
+			// destructive side-effect.
+			classes._Changes?.Remove(classRowId); // Unmarks as changed
+			classes.Remove(classRowId); // Unmarks as held
+		}
+	}
+
+	public void UnloadClassIds() {
+		var classes = _Classes;
+		if (classes != null) {
+			classes._Changes = null; // Unmarks as changed
+			classes.Clear(); // Unmarks as held
+		}
+	}
+
+	// --
+
+	/// <summary>
+	/// Caches the specified class rowid if the fielded entity possesses the
+	/// class.
+	/// </summary>
+	/// <remarks>
+	/// CONTRACT:
+	/// <br/>- Must be called while inside a transaction (ideally, using <see cref="OptionalReadTransaction"/>
+	/// or <see cref="NestingWriteTransaction"/>).
+	/// <br/>- Must load <see cref="_SchemaRowId"/> beforehand, at least once,
+	/// while inside the transaction.
+	/// <para>
+	/// Violation of the above contract may result in undefined behavior.
+	/// </para>
+	/// </remarks>
+	[SkipLocalsInit]
+	private protected bool InternalLoadClassId(KokoroSqliteDb db, long classRowId) {
+		using var cmd = db.CreateCommand();
+		cmd.Set(
+			"SELECT 1 FROM SchemaToClass\n" +
+			"WHERE (schema,cls)=($schema,$cls)"
+		).AddParams(
+			new("$schema", _SchemaRowId),
+			new("$cls", classRowId)
+		);
+
+		using var r = cmd.ExecuteReader();
+		if (r.Read()) {
+			AddClassAsLoaded(classRowId);
+			return true;
+		}
+
+		return false;
+	}
+
+	/// <remarks>
+	/// CONTRACT:
+	/// <br/>- Must be called while inside a transaction (ideally, using <see cref="OptionalReadTransaction"/>
+	/// or <see cref="NestingWriteTransaction"/>).
+	/// <br/>- Must load <see cref="_SchemaRowId"/> beforehand, at least once,
+	/// while inside the transaction.
+	/// <para>
+	/// Violation of the above contract may result in undefined behavior.
+	/// </para>
+	/// </remarks>
+	[SkipLocalsInit]
+	private protected void InternalLoadClassIds(KokoroSqliteDb db) {
+		var classes = _Classes;
+		if (classes == null) {
+			_Classes = classes = new();
+		} else {
+			classes._Changes = null; // Unmarks as changed
+			classes.Clear(); // Unmarks as held
+		}
+
+		using var cmd = db.CreateCommand();
+		cmd.Set("SELECT cls FROM SchemaToClass WHERE schema=$schema")
+			.AddParams(new("$schema", _SchemaRowId));
+
+		var r = cmd.ExecuteReader();
+		r.DAssert_Name(0, "cls");
+
+		while (r.Read()) {
+			long cls = r.GetInt64(0);
+			classes.Add(cls); // Marks as held
+		}
+	}
 }
