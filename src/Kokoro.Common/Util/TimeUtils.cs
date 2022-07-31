e067a83accd7023a611766fc104f5ea285c75c5c
netaneru <netaneru3@gmail.com>
2022-07-31 12:23:15 +0800

    Optimally get the current Unix time in ms

---
+++ src/Kokoro.Common/Util/TimeUtils.cs
@@ -0,0 +1,24 @@
+﻿namespace Kokoro.Common.Util;
+
+internal static class TimeUtils {
+
+	/// <summary>
+	/// The Unix epoch (1970-01-01 00:00 UTC) as the number of milliseconds
+	/// since the BCL epoch (0001-01-01 00:00 UTC).
+	/// </summary>
+	private const long UnixEpochMilliseconds = 62_135_596_800_000;
+
+	[MethodImpl(MethodImplOptions.AggressiveOptimization)]
+	[SkipLocalsInit]
+	public static long UnixMillisNow() {
+		return DateTime.UtcNow.Ticks / TimeSpan.TicksPerMillisecond - UnixEpochMilliseconds;
+		//
+		// Explanation from `DateTimeOffset.ToUnixTimeMilliseconds()`:
+		//
+		//   > Truncate sub-millisecond precision before offsetting by the Unix
+		//   > epoch to avoid the last digit being off by one for dates that
+		//   > result in negative Unix times.
+		//
+		// https://github.com/dotnet/runtime/blob/v6.0.7/src/libraries/System.Private.CoreLib/src/System/DateTimeOffset.cs#L629
+	}
+}
