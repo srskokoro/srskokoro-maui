14208135bbdacf56aafcb2d3927bcd3b632c7b20
netaneru <netaneru3@gmail.com>
2022-08-24 01:30:56 +0800

    Ensured no active transaction prior recycling
    - i.e., prior to `KokoroCollection` disposal completion.

--- src/Kokoro/Internal/Sqlite/KokoroSqliteDb._.cs
+++ src/Kokoro/Internal/Sqlite/KokoroSqliteDb._.cs
@@ -86,6 +86,10 @@ internal sealed partial class KokoroSqliteDb : SqliteConnection {
 	}
 
 	internal void TearDown() {
+		if (HasActiveTransaction) {
+			this.Exec("ROLLBACK");
+		}
+
 		Context = null;
 		Owner = null;
 
