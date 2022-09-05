f3fdb56a4fd5275e6745f7f8e37c77467e954679
netaneru <netaneru3@gmail.com>
2022-09-05 19:49:42 +0800

    Easily set up `KokoroCollection` fixtures in tests

---
+++ test/Kokoro.Test/Fixtures/KokoroCollectionFixtureBase.cs
@@ -0,0 +1,29 @@
+﻿namespace Kokoro.Fixtures;
+using Kokoro.Common.IO;
+
+public abstract class KokoroCollectionFixtureBase : IDisposable {
+	private const string CommonParentDir = "test_kcols";
+
+	public readonly KokoroCollection Collection;
+
+	public KokoroCollectionFixtureBase(ulong uidHighBits, ulong uidLowBits) : this(new(highBits: uidHighBits, uidLowBits)) { }
+
+	public KokoroCollectionFixtureBase(UniqueId uid) {
+		var context = new KokoroContext(Path.Join(CommonParentDir, uid.ToFsString()), KokoroContextOpenMode.ReadWriteCreate);
+		Collection = new KokoroCollection(context);
+	}
+
+	public virtual void Dispose() {
+		var collection = Collection;
+		var context = collection.ContextOrNull;
+		if (context != null) {
+			collection.Dispose();
+
+			string path = context.BasePath;
+			context.Dispose();
+			FsUtils.DeleteDirectoryIfExists(path);
+
+			GC.SuppressFinalize(this);
+		}
+	}
+}
