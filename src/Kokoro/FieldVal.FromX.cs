d8d25665e40e68330f1c451eb46bc33d7c4e954b
netaneru <netaneru3@gmail.com>
2022-09-05 21:20:06 +0800

    Initial conversion methods

--- src/Kokoro/FieldVal.FromX.cs
+++ src/Kokoro/FieldVal.FromX.cs
@@ -1,4 +1,8 @@
 namespace Kokoro;
 
 public sealed partial class FieldVal {
+
+	public static FieldVal From(string text) => new(FieldTypeHint.Text, text.ToUTF8Bytes());
+
+	public static FieldVal From(byte[] blob) => new(FieldTypeHint.Blob, blob);
 }
--- src/Kokoro/FieldVal.ToX.cs
+++ src/Kokoro/FieldVal.ToX.cs
@@ -1,4 +1,70 @@
 namespace Kokoro;
+using System.Buffers.Binary;
+using System.Globalization;
+using System.Numerics;
 
 public sealed partial class FieldVal {
+
+	[SkipLocalsInit]
+	public string? GetText() {
+		FieldTypeHint type = _TypeHint;
+		if (type == FieldTypeHint.Text) {
+			return TextUtils.UTF8ToString(_Data);
+		} else if (type == FieldTypeHint.Null) {
+			return null;
+		} else {
+			return ForceDecodeAsText(type, _Data);
+		}
+	}
+
+	[SkipLocalsInit]
+	private static string ForceDecodeAsText(FieldTypeHint type, byte[] data) {
+		string r;
+		goto Switch;
+
+	Return: // Ensures epilogue happens here instead
+		return r;
+
+	Switch:
+		switch (type) {
+			default: {
+				r = TextUtils.UTF8ToString(data);
+				goto Return;
+			}
+
+			case FieldTypeHint.Zero: { r = "0"; goto Return; }
+			case FieldTypeHint.One: { r = "1"; goto Return; }
+
+			case FieldTypeHint.Int:
+			case FieldTypeHint.UInt: {
+				Debug.Assert((FieldTypeHintInt)FieldTypeHint.Int == 0x4);
+				Debug.Assert((FieldTypeHintInt)FieldTypeHint.UInt == 0x5);
+				r = new BigInteger(
+					value: data,
+					isUnsigned: ((FieldTypeHintInt)type & 1) != 0,
+					isBigEndian: true
+				).ToString(NumberFormatInfo.InvariantInfo);
+				goto Return;
+			}
+
+			case FieldTypeHint.Fp16: {
+				Half value = BinaryPrimitives.ReadHalfBigEndian(data);
+				r = value.ToString(NumberFormatInfo.InvariantInfo);
+				goto Return;
+			}
+			case FieldTypeHint.Fp32: {
+				float value = BinaryPrimitives.ReadSingleBigEndian(data);
+				r = value.ToString(NumberFormatInfo.InvariantInfo);
+				goto Return;
+			}
+			case FieldTypeHint.Fp64: {
+				double value = BinaryPrimitives.ReadDoubleBigEndian(data);
+				r = value.ToString(NumberFormatInfo.InvariantInfo);
+				goto Return;
+			}
+		}
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	public ReadOnlySpan<byte> GetBlob() => Data;
 }
