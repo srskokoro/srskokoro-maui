0f6dc469e13f6e457fbb6ac884d1e27a52e594bf
netaneru <netaneru3@gmail.com>
2022-09-06 04:03:24 +0800

    Optimized: store efficiently instead

--- src/Kokoro/FieldVal.FromX.cs
+++ src/Kokoro/FieldVal.FromX.cs
@@ -24,11 +24,28 @@ public sealed partial class FieldVal {
 
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	[SkipLocalsInit]
-	private static byte[] MakeData<T>(T value) where T : unmanaged {
-		var data = new byte[U.SizeOf<T>()];
-		U.WriteUnaligned(ref data.DangerousGetReference(), value);
+	private static byte[] MakeData<T>(T value, int count) where T : unmanaged {
+		Debug.Assert(count <= U.SizeOf<T>());
+		var data = new byte[count];
+		U.CopyBlockUnaligned(
+			destination: ref data.DangerousGetReference(),
+			source: ref U.As<T, byte>(ref value),
+			byteCount: (uint)count
+		);
 		return data;
 	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private static byte[] MakeData(int value) => MakeData((uint)value);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private static byte[] MakeData(long value) => MakeData((ulong)value);
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private static byte[] MakeData(uint value) => MakeData(value.BigEndian(), value.CountBytesNeeded());
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	private static byte[] MakeData(ulong value) => MakeData(value.BigEndian(), value.CountBytesNeeded());
 
 	// --
 
@@ -40,25 +57,25 @@ public sealed partial class FieldVal {
 
 	public static FieldVal From(sbyte value) {
 		const FieldTypeHint Type = FieldTypeHint.Int;
-		if ((byte)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((byte)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
 	public static FieldVal From(short value) {
 		const FieldTypeHint Type = FieldTypeHint.Int;
-		if ((ushort)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((ushort)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
 	public static FieldVal From(int value) {
 		const FieldTypeHint Type = FieldTypeHint.Int;
-		if ((uint)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((uint)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
 	public static FieldVal From(long value) {
 		const FieldTypeHint Type = FieldTypeHint.Int;
-		if ((ulong)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((ulong)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
@@ -66,25 +83,25 @@ public sealed partial class FieldVal {
 
 	public static FieldVal From(byte value) {
 		const FieldTypeHint Type = FieldTypeHint.UInt;
-		if ((byte)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((byte)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
 	public static FieldVal From(ushort value) {
 		const FieldTypeHint Type = FieldTypeHint.UInt;
-		if ((ushort)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((ushort)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
 	public static FieldVal From(uint value) {
 		const FieldTypeHint Type = FieldTypeHint.UInt;
-		if ((uint)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((uint)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
 	public static FieldVal From(ulong value) {
 		const FieldTypeHint Type = FieldTypeHint.UInt;
-		if ((ulong)value > 1u) return new(Type, MakeData(value.BigEndian()));
+		if ((ulong)value > 1u) return new(Type, MakeData(value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
