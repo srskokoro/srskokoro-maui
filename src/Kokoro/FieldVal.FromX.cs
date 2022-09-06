cea32c8c8c4a0f6acebe071bb4602fbceeda593e
netaneru <netaneru3@gmail.com>
2022-09-06 17:31:13 +0800

    Oops: should use unsigned counterpart

--- src/Kokoro/FieldVal.FromX.cs
+++ src/Kokoro/FieldVal.FromX.cs
@@ -98,7 +98,7 @@ public sealed partial class FieldVal {
 
 	public static FieldVal From(ushort value) {
 		const FieldTypeHint Type = FieldTypeHint.UInt;
-		if ((ushort)value > 1u) return new(Type, MakeData(value));
+		if ((ushort)value > 1u) return new(Type, MakeData((uint)value));
 		return ZeroOrOneInstHolder.DangerousGetZeroOrOne((int)value);
 	}
 
