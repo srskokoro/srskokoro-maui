a1b27b482151de1c88e1f2e9a99d875aab6c9f37
netaneru <netaneru3@gmail.com>
2022-09-08 18:34:05 +0800

    No longer necessary

--- src/Kokoro/FieldTypeHint.cs
+++ src/Kokoro/FieldTypeHint.cs
@@ -6,12 +6,12 @@ namespace Kokoro;
 using Kokoro.Common.Util;
 
 public enum FieldTypeHint : FieldTypeHintInt {
-	Null     = 0x0,
+	Null = 0x0,
 
 	/// <summary>The integer zero.</summary>
-	Zero     = 0x2,
+	Zero = 0x2,
 	/// <summary>The integer one.</summary>
-	One      = 0x3,
+	One = 0x3,
 
 	/// <summary>
 	/// A little-endian arbitrary-length signed integer.
@@ -20,7 +20,7 @@ public enum FieldTypeHint : FieldTypeHintInt {
 	/// Conversion to C#'s primitive integer types may cause the value to be
 	/// either truncated or sign-extended. Empty data is interpreted as zero.
 	/// </remarks>
-	Int      = 0x4,
+	Int = 0x4,
 	/// <summary>
 	/// A little-endian arbitrary-length unsigned integer.
 	/// </summary>
@@ -28,7 +28,7 @@ public enum FieldTypeHint : FieldTypeHintInt {
 	/// Conversion to C#'s primitive integer types may cause the value to be
 	/// either truncated or zero-extended. Empty data is interpreted as zero.
 	/// </remarks>
-	UInt     = 0x5,
+	UInt = 0x5,
 
 	/// <summary>
 	/// A little-endian IEEE 754 64-bit floating-point number.
@@ -37,13 +37,13 @@ public enum FieldTypeHint : FieldTypeHintInt {
 	/// Excess data bytes will be discarded. Less than 8 bytes (or 64 bits) of
 	/// data will be interpreted as NaN.
 	/// </remarks>
-	Real     = 0x8,
+	Real = 0x8,
 
 	/// <summary>A UTF-8 string.</summary>
 	// TODO A zipped UTF-8 counterpart. See, https://utf8everywhere.org/#asian
-	Text     = 0x54,
+	Text = 0x54,
 
-	Blob     = 0x58,
+	Blob = 0x58,
 
 	/// <remarks>
 	/// Note: <c>(0x68 + 16) * 2 == 240</c>, which occupies a single byte when
