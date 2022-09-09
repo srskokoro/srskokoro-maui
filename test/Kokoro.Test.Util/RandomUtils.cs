f3a3ae23e73024510a0e5e751192b96d2ed74eb0
netaneru <netaneru3@gmail.com>
2022-09-09 22:53:31 +0800

    Utility to easily generate uniform random bits

---
+++ test/Kokoro.Test.Util/RandomUtils.cs
@@ -0,0 +1,14 @@
+﻿namespace Kokoro.Test.Util;
+
+public static class RandomUtils {
+
+	public static uint NextUniform32(this Random random) {
+		// See, https://stackoverflow.com/a/18332307
+		return (uint)random.NextInt64(1L << 32);
+	}
+
+	public static ulong NextUniform64(this Random random) {
+		// See, https://stackoverflow.com/a/18332307
+		return (ulong)((random.NextInt64(1L << 62) << 2) | random.NextInt64(1L << 2));
+	}
+}
