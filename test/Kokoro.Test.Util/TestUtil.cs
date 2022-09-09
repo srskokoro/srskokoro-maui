dd30bdf826e3060e601724b86102eb39a20dc758
netaneru <netaneru3@gmail.com>
2022-09-09 23:54:04 +0800

    Moved to a more apt place

--- test/Kokoro.Test.Util/TestUtil.cs
+++ test/Kokoro.Test.Util/TestUtil.cs
@@ -14,39 +14,6 @@ public static partial class TestUtil {
 	public static Random GetRandom<T>(this T _) where T : class, IRandomizedTest
 		=> LocalRandomAccess.GetRandom<T>();
 
-	public static Span<byte> Init(this Random random, Span<byte> bytes) {
-		random.NextBytes(bytes);
-		return bytes;
-	}
-
-	public static Span<char> InitAscii(this Random random, Span<char> chars) {
-		for (int i = 0; i < chars.Length; i++) {
-			chars[i] = random.NextAscii();
-		}
-		return chars;
-	}
-
-	public static string MakeAsciiStr(this Random random, int length) {
-		return string.Create(length, random, static (chars, random) => random.InitAscii(chars));
-	}
-
-	public static string MakeAsciiStr(this Random random, int minLength, int maxLength) {
-		return random.MakeAsciiStr(random.Next(minLength, maxLength + 1));
-	}
-
-
-	public static char NextAscii(this Random random) {
-		return (char)random.Next(32, 127);
-	}
-
-	public static T NextItem<T>(this Random random, T[] array) {
-		return array[random.Next(array.Length)];
-	}
-
-	public static TEnum NextEnum<TEnum>(this Random random) where TEnum : struct, Enum {
-		return random.NextItem(EnumUtils.EnumVals<TEnum>.Values);
-	}
-
 
 	public static void RandomSpin(int minSpinCount, int maxSpinCount) {
 		DoSpin(Random.Shared.Next(minSpinCount, maxSpinCount));
