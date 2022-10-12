ad55104f8506bf12dca2f886cf4251cfb4e63e32
netaneru <netaneru3@gmail.com>
2022-10-12 16:33:14 +0900

    Implemented `IEquatable<FieldVal>`

--- src/Kokoro/FieldVal._.cs
+++ src/Kokoro/FieldVal._.cs
@@ -2,7 +2,7 @@
 using Blake2Fast.Implementation;
 using Kokoro.Common.Util;
 
-public sealed partial class FieldVal {
+public sealed partial class FieldVal : IEquatable<FieldVal> {
 
 	public static FieldVal Null => NullInstHolder.Instance;
 
@@ -12,6 +12,7 @@ public sealed partial class FieldVal {
 
 	private readonly FieldTypeHint _TypeHint;
 	private readonly byte[] _Data;
+	private int _HashCode;
 
 	public FieldTypeHint TypeHint => _TypeHint;
 
@@ -20,6 +21,7 @@ public sealed partial class FieldVal {
 		get => _Data;
 	}
 
+	/// <seealso cref="DangerousResetHashCode()"/>
 	[MethodImpl(MethodImplOptions.AggressiveInlining)]
 	internal byte[] DangerousGetDataBytes() => _Data;
 
@@ -76,4 +78,62 @@ public sealed partial class FieldVal {
 			hasher.Update(data);
 		}
 	}
+
+	#region Equality
+
+	[MethodImpl(MethodImplOptions.AggressiveOptimization)]
+	public override bool Equals([NotNullWhen(true)] object? obj) {
+		if ((object)this != obj) {
+			if (obj is null) goto NE;
+			if (obj is not FieldVal other) goto NE;
+			if (_TypeHint != other._TypeHint) goto NE;
+			if (!_Data.AsDangerousROSpan().SequenceEqual(
+				other._Data.AsDangerousROSpan())) goto NE;
+		}
+		return true;
+	NE:
+		return false;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveOptimization)]
+	public bool Equals([NotNullWhen(true)] FieldVal? other) {
+		if ((object)this != other) {
+			if (other is null) goto NE;
+			if (_TypeHint != other._TypeHint) goto NE;
+			if (!_Data.AsDangerousROSpan().SequenceEqual(
+				other._Data.AsDangerousROSpan())) goto NE;
+		}
+		return true;
+	NE:
+		return false;
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	[SkipLocalsInit]
+	public override int GetHashCode() {
+		int h = _HashCode;
+		if (h != 0) return h;
+		return ComputeHashCode();
+	}
+
+	[MethodImpl(MethodImplOptions.AggressiveOptimization)]
+	[SkipLocalsInit]
+	private int ComputeHashCode() {
+		HashCode h = new();
+		h.Add(_TypeHint);
+		h.AddBytes(_Data.AsDangerousROSpan());
+		return _HashCode = h.ToHashCode();
+		// NOTE: AFAIK, the above is still thread-safe :P
+		// - The worst that can happen is that each thread would have to
+		// recompute the hash code.
+	}
+
+	/// <remarks>
+	/// For when the underlying data (returned by <see cref="DangerousGetDataBytes()"/>)
+	/// were modified.
+	/// </remarks>
+	[MethodImpl(MethodImplOptions.AggressiveInlining)]
+	internal void DangerousResetHashCode() => _HashCode = 0;
+
+	#endregion
 }
